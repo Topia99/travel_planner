8e39cf4838d887f9752dfa7e489a39e282067741
potato100861 <y1737647841@gmail.com>
2024-10-03 18:47:36 -0700

    fix bugs

--- Travel-Planner/Travel-Planner/Views/TripListPageViews/CityCardView.swift
+++ Travel-Planner/Travel-Planner/Views/TripListPageViews/CityCardView.swift
@@ -3,66 +3,7 @@
 //  Travel-Planner
 //
 //  Created by siye yang on 10/3/24.
-//
-//
-//import SwiftUI
-//
-//struct CityCardView: View {
-//    var cityName: String
-//    var countryName: String
-//    var imageName: String
-//    
-//    var body: some View {
-//       
-//        VStack {
-//            // Image with overlay
-//            Image(imageName)
-//                .resizable()
-//                .aspectRatio(contentMode: .fill)
-//                .frame(height: 150)
-//                .cornerRadius(15)
-//                .clipped()
-//                // Overlay the RoundedRectangle on the image
-//                .overlay(
-//                    RoundedRectangle(cornerRadius: 15)
-//                        .fill(Color.black.opacity(0.3))
-//                )
-//                // Overlay the text and button on top of the image and rectangle
-//                .overlay(
-//                    VStack(alignment: .leading, spacing: 5) {
-//                        Text(cityName)
-//                            .font(.title)
-//                            .bold()
-//                            .foregroundColor(.white)
-//                        
-//                        Text(countryName)
-//                            .font(.subheadline)
-//                            .foregroundColor(.white)
-//                        
-//                        Spacer()
-//                        
-//                        // Edit button
-//                        Button{
-//                            // action
-//                        }label: {
-//                            Text("Edit")
-//                                .font(.caption)
-//                                .foregroundColor(.white)
-//                                .padding(.horizontal, 10)
-//                                .padding(.vertical, 5)
-//                                .background(Color.gray)
-//                                .cornerRadius(10)
-//                        }
-//                        .padding(.bottom, 10)
-//                    }
-//                    .padding(.leading, 15)
-//                    .padding(.vertical, 15),
-//                    alignment: .bottomLeading
-//                )
-//        }
-//        .shadow(radius: 5)
-//    }
-//}
+
 import SwiftUI
 
 struct CityCardView: View {
