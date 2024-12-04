ec74b1fc5e9e57506e868d6daa2983f228c81218
potato100861 <y1737647841@gmail.com>
2024-12-03 18:54:21 -0800

    FixedBug

--- Travel-Planner/Travel-Planner/Screens/TripListView.swift
+++ Travel-Planner/Travel-Planner/Screens/TripListView.swift
@@ -10,24 +10,46 @@ import SwiftUI
 struct TripListView: View {
     @StateObject var vm = TripViewModel()
     @State private var showAddTrip: Bool = false
-    
+    @State private var deleteMode: Bool = false
+
     var body: some View {
         NavigationStack {
-            ScrollView{
+            ScrollView {
                 LazyVStack {
-                    ForEach(vm.trips) { trip in
-                        NavigationLink(destination: TripView(vm: DayPlanViewModel(trip: trip))) {
-                            Text(trip.title)
-                        }
+                    ForEach(vm.trips, id: \.self) { trip in
+                        TripListItemView(
+                            trip: trip,
+                            deleteMode: $deleteMode,
+                            deleteAction: deleteTrip)
                     }
                 }
-                .navigationTitle("Trips")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            showAddTrip.toggle()
+                .padding(.horizontal, 16)
+                .padding(.top, 10)
+            }
+            .navigationTitle(deleteMode ? "Delete Trips" : "Trips")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if deleteMode {
+                        // Show "Done" button to exit delete mode
+                        Button("Done") {
+                            deleteMode = false
+                        }
+                        .foregroundColor(.blue)
+                    } else {
+                        // Show a menu with "Add" and "Delete" options
+                        Menu {
+                            Button(action: {
+                                showAddTrip.toggle()
+                            }) {
+                                Label("Add", systemImage: "plus")
+                            }
+                            Button(action: {
+                                deleteMode.toggle()
+                            }) {
+                                Label("Delete", systemImage: "trash")
+                            }
                         } label: {
-                            Image(systemName: "plus")
+                            Image(systemName: "ellipsis.circle")
                         }
                     }
                 }
@@ -39,7 +61,39 @@ struct TripListView: View {
             }
         }
     }
+
+    private func deleteTrip(_ trip: TripEntity) {
+        vm.deleteTrip(trip: trip)
+    }
 }
+
+struct TripListItemView: View {
+    var trip: TripEntity
+    @Binding var deleteMode: Bool
+    var deleteAction: (TripEntity) -> Void
+
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            NavigationLink(destination: TripView(vm: DayPlanViewModel(trip: trip))) {
+                TripCardView(trip: trip)
+            }
+            .buttonStyle(PlainButtonStyle())
+            .disabled(deleteMode)
+
+            if deleteMode {
+                Button(action: {
+                    deleteAction(trip)
+                }) {
+                    Image(systemName: "minus.circle.fill")
+                        .foregroundColor(.red)
+                        .font(.title)
+                }
+                .padding([.top, .trailing], 10)
+            }
+        }
+    }
+}
+
 
 #Preview {
     TripListView()
---
+++ Travel-Planner/Travel-Planner/Views/AddTripView/Calendar/CustomCalendarView.swift
@@ -0,0 +1,146 @@
+//
+//  CustomCalendarView.swift
+//  TravelPlanner
+//
+//  Created by sam yang on 9/24/24.
+//
+
+import SwiftUI
+
+struct CustomCalendarView: View {
+    @Binding var startDate: Date?
+    @Binding var endDate: Date?
+
+    @State private var currentMonth: Date = Date()
+
+    let calendar = Calendar.current
+    let dateFormatter = DateFormatter()
+
+    var body: some View {
+        VStack {
+            // Month Navigation
+            HStack {
+                Button(action: {
+                    previousMonth()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .font(.title2)
+                        .frame(width: 44, height: 44)
+                        .contentShape(Rectangle())
+                }
+                .buttonStyle(PlainButtonStyle())
+
+                Spacer()
+
+                Text(monthYearString(from: currentMonth))
+                    .font(.headline)
+
+                Spacer()
+
+                Button(action: {
+                    nextMonth()
+                }) {
+                    Image(systemName: "chevron.right")
+                        .font(.title2)
+                        .frame(width: 44, height: 44)
+                        .contentShape(Rectangle())
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            .padding(.horizontal)
+
+            // Days of the Week Labels
+            HStack {
+                ForEach(calendar.shortWeekdaySymbols, id: \.self) { day in
+                    Text(day)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                }
+            }
+
+            // Dates Grid
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
+                ForEach(daysInCurrentMonth, id: \.self) { date in
+                    DateCellView(date: date, startDate: $startDate, endDate: $endDate, currentMonth: currentMonth)
+                        .onTapGesture {
+                            handleDateTap(date)
+                        }
+                }
+            }
+        }
+    }
+
+    // Computed property for days in the current month
+    var daysInCurrentMonth: [Date] {
+        generateDaysInMonth(for: currentMonth)
+    }
+
+    // Helper methods
+    func generateDaysInMonth(for date: Date) -> [Date] {
+        guard let monthInterval = calendar.dateInterval(of: .month, for: date),
+              let firstWeek = calendar.dateInterval(of: .weekOfMonth, for: monthInterval.start) else {
+            return []
+        }
+
+        var days: [Date] = []
+        var currentDate = firstWeek.start
+
+        // Include dates from the previous and next months to fill the calendar grid
+        while currentDate <= monthInterval.end || calendar.isDate(currentDate, equalTo: monthInterval.end, toGranularity: .day) {
+            days.append(currentDate)
+            currentDate = calendar.date(byAdding: .day, value: 1, to: currentDate)!
+        }
+
+        return days
+    }
+
+    func monthYearString(from date: Date) -> String {
+        dateFormatter.dateFormat = "LLLL yyyy"
+        return dateFormatter.string(from: date)
+    }
+
+    func previousMonth() {
+        if let newDate = calendar.date(byAdding: .month, value: -1, to: currentMonth) {
+            currentMonth = newDate
+        }
+    }
+
+    func nextMonth() {
+        if let newDate = calendar.date(byAdding: .month, value: 1, to: currentMonth) {
+            currentMonth = newDate
+        }
+    }
+
+    // Updated handleDateTap function
+    func handleDateTap(_ date: Date) {
+        if calendar.compare(date, to: Date(), toGranularity: .day) == .orderedAscending {
+            // Do not allow selection of past dates (optional)
+            return
+        }
+        if startDate == nil {
+            // First tap sets the start date
+            startDate = date
+            endDate = nil
+        } else if endDate == nil {
+            // Second tap
+            if date <= startDate! {
+                // Update startDate to the earlier date
+                startDate = date
+            } else if date > startDate! {
+                // Set endDate
+                endDate = date
+            } else {
+                // Date is the same as startDate, do nothing or reset selection
+                // For this example, we will do nothing
+            }
+        } else {
+            // If both startDate and endDate are set, reset the selection
+            startDate = date
+            endDate = nil
+        }
+    }
+}
+
+#Preview {
+    CustomCalendarView(startDate: .constant(Date()), endDate: .constant(Date()))
+}
